7277065221bf757fba0b4d9066a0a794d3869247
SkripnikRostislav <skrypnik-r@ukr.net>
2018-12-30 13:40:17 +0200

    Refactored Add method

--- GitTest/Feature1.cs
+++ GitTest/Feature1.cs
@@ -6,12 +6,9 @@ namespace GitTest
 {
     class Feature1
     {
-        public int Add()
-        {
-            int x1 = 1;
-            int x2 = 2;
-            int sum = x1 + x2;
-            return sum;
+        public int Add(int x1, int x2)
+        {            
+            return x1+x2;
         }
     }
 }
